a8e2aa0e201d47514c00ada6160febaf97381aeb
Brandon Furtwangler <brandf@fb.com>
2023-06-02 18:20:15 -0700

    adding perf test for different mesh types

--- Assets/Tests/PerformanceTests.cs
+++ Assets/Tests/PerformanceTests.cs
@@ -7,9 +7,35 @@ namespace SliceyMesh
 
     public class PerformanceTests : TestBase
     {
+        [Test, Performance]
+        public void MeshTypeNoCache([Values(SliceyMesh.SliceyMeshType.CuboidHard, 
+                                            SliceyMesh.SliceyMeshType.CuboidCylindrical, 
+                                            SliceyMesh.SliceyMeshType.CuboidSpherical)] SliceyMesh.SliceyMeshType type)
+        {
+            var config = new SliceyConfig()
+            {
+                Type = type,
+                Size = new Vector3(1, 1, 1),
+                Radii = new Vector4(0.25f, 0f, 0f, 0f),
+                Quality = 1.0f,
+            };
+
+            Measure.Method(() =>
+            {
+                Cache.Get(config, SliceyMaterialFlags.ShaderSlicingNotSupported);
+            }).
+            SetUp(() =>
+            {
+                Cache.Clear();
+            }).
+            WarmupCount(5).
+            IterationsPerMeasurement(1).
+            MeasurementCount(100).
+            Run();
+        }
 
         [Test, Performance]
-        public void CacheMiss([Values(0.25f, 0.5f, 0.75f, 1.0f)] float quality)
+        public void CacheMiss([Values(0.0f, 0.5f, 1.0f, 2.0f, 3.0f)] float quality)
         {
             var config = new SliceyConfig()
             {
@@ -34,7 +60,7 @@ namespace SliceyMesh
         }
 
         [Test, Performance]
-        public void CacheHit([Values(0.25f, 0.5f, 0.75f, 1.0f)] float quality)
+        public void CacheHit([Values(0.0f, 0.5f, 1.0f, 2.0f, 3.0f)] float quality)
         {
             var config = new SliceyConfig()
             {
@@ -56,7 +82,7 @@ namespace SliceyMesh
         }
 
         [Test, Performance]
-        public void CanonicalCacheHit([Values(0.25f, 0.5f, 0.75f, 1.0f)] float quality)
+        public void CanonicalCacheHit([Values(0.0f, 0.5f, 1.0f, 2.0f, 3.0f)] float quality)
         {
             var configA = new SliceyConfig()
             {
